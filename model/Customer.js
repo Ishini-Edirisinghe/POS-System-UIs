a866eb50fe82d1c9a321c66b9f70e1557d197c96
Ishini Edirisinghe <ishiniedirisinghe331@gmail.com>
2023-11-10 00:18:32 +0530

    modification

--- model/Customer.js
+++
@@ -1,8 +0,0 @@
-export class Customer {
-    constructor(cusId, cusName, cusAddress, cusSalary) {
-        this.cusId = cusId;
-        this.cusName = cusName;
-        this.cusAddress = cusAddress;
-        this.cusSalary = cusSalary;
-    }
-}
--- model/Item.js
+++
@@ -1,8 +0,0 @@
-export class Item{
-    constructor(itemId, itemName, itemPrice, itemQty){
-        this.itemId = itemId;
-        this.itemName = itemName;
-        this.itemPrice = itemPrice;
-        this.itemQty = itemQty;
-    }
-}
